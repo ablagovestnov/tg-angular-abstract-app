e492e29879442f966db4056cb6b1ddcb345e01e9
Alexandr Blagovestnov <a.blagovestnov@dcoa.ru>
2024-06-17 19:55:34 +0300

    wip

--- get_cert.sh
+++ get_cert.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Получаем сертификат с помощью certbot
-certbot --nginx -d adventure-finder.com -d www.adventure-finder.com --non-interactive --agree-tos --email  a.blagovestnov@gmail.com
+certbot certonly --dry-run --nginx -d adventure-finder.com -d www.adventure-finder.com --non-interactive --agree-tos --email  a.blagovestnov@gmail.com

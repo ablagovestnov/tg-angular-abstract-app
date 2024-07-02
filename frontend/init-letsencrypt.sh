f3451d2e26af5da10cc739cb39e2359b6843cefb
Alexandr Blagovestnov <a.blagovestnov@dcoa.ru>
2024-07-02 15:13:41 +0300

    wip

--- frontend/init-letsencrypt.sh
+++ frontend/init-letsencrypt.sh
@@ -4,15 +4,15 @@ domain="adventure-finder.com"
 email="a.blagovestnov@gmail.com"
 staging=1 # 1 для тестирования, 0 для реального сертификата
 
-if [ $staging != "0" ]; then
-    certbot_args="--staging"
-else
-    certbot_args=""
-fi
-
-# Получение сертификата с использованием Certbot
-#certbot --nginx -d $domain -d www.$domain --non-interactive --agree-tos --email $email $certbot_args
-certbot --nginx -d adventure-finder.com -d www.adventure-finder.com --non-interactive --agree-tos --email a.blagovestnov@gmail.com --staging
-
-# Перезапуск Nginx для применения новой конфигурации
-nginx -s reload
+#if [ $staging != "0" ]; then
+#    certbot_args="--staging"
+#else
+#    certbot_args=""
+#fi
+#
+## Получение сертификата с использованием Certbot
+##certbot --nginx -d $domain -d www.$domain --non-interactive --agree-tos --email $email $certbot_args
+#certbot --nginx -d adventure-finder.com -d www.adventure-finder.com --non-interactive --agree-tos --email a.blagovestnov@gmail.com --staging
+#
+## Перезапуск Nginx для применения новой конфигурации
+#nginx -s reload
